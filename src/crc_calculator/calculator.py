7ce2d133fcadb800e42831653164a7d93455cafb
Jeonghun-Ban <junghoon.ban@gmail.com>
2022-10-26 12:46:13 +0900

    feat: Add xor function

--- src/crc_calculator/calculator.py
+++ src/crc_calculator/calculator.py
@@ -2,6 +2,19 @@ from typing import List
 
 
 def calculate(polynomial: List[int], data: List[int]):
+    result_length = len(polynomial) - 1
+    for i in range(result_length):
+        data.append(0)
 
-    print(polynomial)
-    print(data)
+    result = xor(polynomial, data)
+    print(result)
+
+
+def xor(a: List[int], b: List[int]) -> List[int]:
+    """Calculate xor between int list"""
+    result = []
+
+    for i in range(len(a)):
+        result.append(a[i] ^ b[i])
+
+    return result
